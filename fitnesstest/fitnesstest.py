345522378a8b90345a2ffc16b705e98ec9a81df2
Lucy Newman <newmanlucy@uchicago.edu>
2018-02-01 12:25:05 -0600

    added exercise class

--- fitnesstest/fitnesstest.py
+++ fitnesstest/fitnesstest.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 
+from exercise import Exercise
+
 class FitnessTest:
 	def __init__(self, numExercises, category):
 		self.numExercises = numExercises
@@ -7,9 +9,18 @@ class FitnessTest:
 
 	def __repr__(self):
 		string = "FitnessTest: %s, %d exercises" % (self.category, self.numExercises)
-		return string		
+		return string
+
+	def getRPM(self, exercise, numReps):
+		return numReps * 60.0 / exercise.duration
 
 
 if __name__ == '__main__':
 	test = FitnessTest(3, "cardio")
 	print(test)
+
+	ex = Exercise("block jumps", 30.0)
+
+	rpm = test.getRPM(ex, 14)
+	print(ex)
+	print("rpm: %f" % rpm)
