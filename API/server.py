1a32f19ce16e31ad72850c6c947a708c26480ef0
ama291 <ama291@gmail.com>
2018-02-08 12:15:36 -0600

    basic database connectivity

--- API/server.py
+++ API/server.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, Response
 import json
+import sqlite3
 app = Flask(__name__)
 app.config['DEBUG'] = False
+
+#database connections - path to data.db on server
+conn = sqlite3.connect("/Project/Music-Driven-Workout-App/data.db")
+c = conn.cursor()
 
 @app.route('/')
 def index():
@@ -9,4 +14,8 @@ def index():
 
 @app.route('/api')
 def api():
-    return Response(json.dumps({"Status": "Welcome to the API."}), mimetype='application/json')
+    return Response(json.dumps({"Message": "Welcome to the API", "Status" : "Success"}), mimetype='application/json')
+
+@app.route('/api/database/<string:query>')
+def api():
+    return Response(json.dumps({"Query": query, "Status": "Success"}), mimetype='application/json')
