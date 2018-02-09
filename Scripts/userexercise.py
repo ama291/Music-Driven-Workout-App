470ed29d8c8d11a7cb74503448b43898e39e0fba
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2018-02-09 13:04:30 -0600

    added scraper and merged fitnesstest and userexercise

--- Scripts/userexercise.py
+++ Scripts/userexercise.py
@@ -3,10 +3,16 @@ from Scripts.exercise import Exercise
 from datetime import datetime
 
 class UserExercise:
-    def __init__(self, exercise, rpm):
+    def __init__(self, exercise, category, numReps):
         self.exercise = exercise
         self.trials = []
+        self.rpm = self.getRPM(exercise, numReps)
         self.addTrial(rpm)
+        if category not in Exercise.categories:
+            raise ValueError("%s is not a valid category")
+        if numExercises <= 0:
+            raise ValueError("numExercises must be positive")
+        self.category = category
 
     def __repr__(self):
         string = "UserExercise: %s, max: %f rpm on %s\n\tTrials:" % \
@@ -32,13 +38,16 @@ class UserExercise:
     def addTrial(self, rpm):
         self.trials.append((datetime.now(),rpm))
 
+    def getRPM(self, exercise, numReps):
+        return numReps * 60.0 / exercise.duration
+
     @property
     def maxRate(self):
         return(max(self.trials, key=lambda x: x[1]))
 
 if __name__ == '__main__':
     ex = Exercise("Lunges", 30.0)
-    uex = UserExercise(ex, 16.0)
+    uex = UserExercise(ex, 16.0, 12)
     for i in [23.5,32.0,12.0]:
         uex.addTrial(i)
     print(uex)
