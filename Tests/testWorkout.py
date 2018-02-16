cfe564af82286abd71e2b1a2578f586c39bf4c7c
larissaclopton <cloptla@gmail.com>
2018-02-15 22:10:54 -0600

    testworkout passes

--- Tests/testWorkout.py
+++ Tests/testWorkout.py
@@ -58,6 +58,10 @@ class TestWorkout(unittest.TestCase):
     for i in range(len(workout1.Exercises)):
       self.assertTrue(workout1.Exercises[i].equipment in equipment)
 
+    # test if each exercise has correct difficulty level
+    for i in range(len(workout1.Exercises)):
+      self.assertEqual(workout1.Exercises[i].difficulty,difficulty)
+
     # muscle group condition
     themes = None
     categories = None
@@ -66,10 +70,6 @@ class TestWorkout(unittest.TestCase):
     duration = 30
     difficulty = "Beginner"
     workout2 = usr1.getWorkout(themes, categories, muscleGroups, equipment, duration, difficulty)
-      
-    #test if each exercise has correct difficulty level
-    for i in range(len(workout1.Exercises)):
-      self.assertEqual(workout1.Exercises[i].difficulty,difficulty)
 
     # test workout properties match input parameters and has exercises
     self.assertEqual(workout2.uid, usr1.ID)
