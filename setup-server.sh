364c7d5d0819dd09454b41a276ef3ce3e611eb37
ama291 <ama291@gmail.com>
2018-02-16 02:06:46 -0600

    simplify setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -2,8 +2,8 @@
 supervisorctl stop flaskapp
 
 #set up venv
-virtualenv -p python3 env
-source env/bin/activate
+#virtualenv -p python3 env
+#source env/bin/activate
 
 #install requirements
 #while read line; do
