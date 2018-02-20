63a2a4695b083606f4e161ffdc146467becf2877
Lucy Newman <newmanlucy@uchicago.edu>
2018-02-20 01:34:28 -0600

    working on returning a dictionary from API functions

--- Scripts/testrequest.py
+++ Scripts/testrequest.py
@@ -4,3 +4,4 @@ import requests
 # Make request to API
 r = requests.post('http://138.197.49.155:5000/api/database/', data = {'query': 'SELECT * FROM users', 'key': 'SoftCon2018'})
 print(r.json())
+
