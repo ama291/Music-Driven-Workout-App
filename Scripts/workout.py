6fadb7e3e8224a1a23b810b1c21de06df9c7691e
larissaclopton <cloptla@gmail.com>
2018-02-21 14:44:50 -0600

    added recommendation prototypes

--- Scripts/workout.py
+++ Scripts/workout.py
@@ -2,16 +2,19 @@
 import uuid
 import requests
 from Scripts.exercise import Exercise
-#from Scripts.userexercise import UserExercise
+import jsonpickle
+
 class Workout(object):
-    def __init__(self, uid, themes, categories, muscleGroups, equipment, duration, difficulty):
+    def __init__(self, uid, spotID, themes, categories, muscleGroups, equipment, duration, difficulty, accessToken):
         self.ID = str(uuid.uuid4())  # random UUID, jsonpickle has strings for dict keys
-        self.themes = themes # will not be used until iter 2, for music recommendations
+        self.themes = themes # for music recommendations
         self.uid = uid # user ID, to get fitness test info
+        self.spotID = spotID # spotify ID, for accessing spotify info
+        self.accessToken = accessToken # for authenticated spotify requests
         self.categories = categories
         self.muscleGroups = muscleGroups
         self.equipment = equipment
-        self.equipment.append("Body Only") #Body Only is always included in list of equipment
+        self.equipment.append("Body Only") # Body Only is always included in list of equipment
         self.duration = duration # updated by generateWorkout
         self.difficulty = difficulty
         self.Exercises = [] # filled by generateWorkout
@@ -24,8 +27,9 @@ class Workout(object):
         limit = 30 # number of exercises for each trial, can be tuned
         trials = 10 # number of runs of algorithm, can be tuned
         results = [[]] * trials
+        # TODO - need to add bpm column to exercise table
         attributes = ['id', 'name', 'type', 'muscle', 'level', 'equipment',
-                        'range_start', 'range_end', 'increment', 'rpm']
+                        'range_start', 'range_end', 'increment', 'rpm', 'bpm', 'images']
         equipment = ', '.join("\'" + e + "\'" for e in self.equipment)
 
         duration = self.duration
@@ -34,9 +38,10 @@ class Workout(object):
             categories = ', '.join("\'" + c + "\'" for c in self.categories)
 
             query = 'SELECT %s FROM exercises WHERE type IN (%s) AND equipment IN (%s) \
-                    AND range_start <= %s AND level = %s ORDER BY RANDOM() LIMIT %s' \
-                    % (', '.join(attributes), categories, equipment, duration, difficulty, str(limit))
-            
+                    AND range_start <= %d AND level = %s ORDER BY RANDOM() LIMIT %d' \
+                    % (', '.join(attributes), categories, equipment, duration, difficulty, limit)
+
+            # get random subset of exercises from query for each trial
             for i in range(trials):
 
                 r = requests.post('http://138.197.49.155:5000/api/database/',
@@ -50,9 +55,10 @@ class Workout(object):
             muscleGroups = ', '.join("\'" + m + "\'" for m in self.muscleGroups)
 
             query = 'SELECT %s FROM exercises WHERE muscle IN (%s) AND equipment \
-                    IN (%s) AND range_start <= %s AND level = %s ORDER BY RANDOM() LIMIT %s' \
-                    % (', '.join(attributes), muscleGroups, equipment, duration, difficulty, str(limit))
+                    IN (%s) AND range_start <= %d AND level = %s ORDER BY RANDOM() LIMIT %d' \
+                    % (', '.join(attributes), muscleGroups, equipment, duration, difficulty, limit)
 
+            # get random subset of exercises from query for each trial
             for i in range(trials):
                 
                 r = requests.post('http://138.197.49.155:5000/api/database/',
@@ -80,12 +86,20 @@ class Workout(object):
         self.Exercises = finalExercises
 
         for ex in self.Exercises:
-            query = 'select rate from userexercises where exID = %s and timestamp \
-             = (select max(timestamp) from userexercises where exID = %s) limit 1' % (str(ex.id), str(ex.id))
+
+            # update rpm/bpm if user has tested on the exercise
+            # TODO - will also need to remap bpm
+            query = 'select rate from userexercises where exID = %d and timestamp \
+             = (select max(timestamp) from userexercises where exID = %d) limit 1' % (ex.id, ex.id)
             r = requests.post('http://138.197.49.155:5000/api/database/',
                               data={'query': query, 'key': 'SoftCon2018'})
             if r.json()["Status"] == "Success" and len(r.json()["Result"]) > 0:
-                ex.rpm = r.json()["Result"][0][0]
+                scale = 0.9 if self.difficulty == "Intermediate" else 0.7
+                ex.rpm = scale * int(r.json()["Result"][0][0])
+
+            # get recommendations based on bpm, with duration >= to exercise duration
+            ex.tracks = self.getRecommendations(self.spotID, self.themes, self.accessToken, ex.bpm, ex.duration)
+
         return True
 
 
@@ -94,10 +108,13 @@ class Workout(object):
         exercises = []
         total_duration = 0
 
+        # add in exercises as long as they do not put over self.duration
         for i in range(m):
             exRange = [0]*2
             for j in range(n):
-                if ordering[j] == "name":
+                if ordering[j] == "id":
+                    id = dbExercises[i][j]
+                elif ordering[j] == "name":
                     name = dbExercises[i][j]
                 elif ordering[j] == "level":
                     difficulty = dbExercises[i][j]
@@ -107,16 +124,20 @@ class Workout(object):
                     muscleGroup = dbExercises[i][j]
                 elif ordering[j] == "equipment":
                     equipment = dbExercises[i][j]
+                elif ordering[j] == "images":
+                    images = dbExercises[i][j]
                 elif ordering[j] == "range_start":
                     exRange[0]= dbExercises[i][j]
                 elif ordering[j] == "range_end":
                     exRange[1] = dbExercises[i][j]
                 elif ordering[j] == "increment":
                     increment = dbExercises[i][j]
-                else: # ordering[j] == "rpm"
+                elif ordering[j] == "rpm":
                     rpm = dbExercises[i][j]
+                else: # bpm
+                    bpm = dbExercises[i][j]
 
-            new = Exercise(id,name, difficulty, category, muscleGroup, equipment, [], exRange, increment, rpm)
+            new = Exercise(id,name, difficulty, category, muscleGroup, equipment, images, exRange, increment, rpm, bpm)
 
             if new.duration + total_duration <= self.duration:
                 exercises.append(new)
@@ -125,3 +146,17 @@ class Workout(object):
                     break
 
         return total_duration, exercises
+
+
+    # TODO - implement this
+    def getRecommendations(self, spotID, themes, accessToken, tempo, duration):
+        # call helper function to generate seeds lists
+        # call recommendations with seeds lists
+        # add exercises in until >= duration
+        # return list of {name: , uri: , duration: } dicts
+        return []
+
+    # TODO - implement this
+    def getSeeds(self, spotID, themes, accessToken):
+        # return {tracks: , artists: , genres: } dict
+        return []
