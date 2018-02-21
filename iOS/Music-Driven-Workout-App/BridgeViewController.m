36ca1cc4fac9fae3d7748045f0b9335bc4a1460c
techbar <techbar@Techbars-MacBook-Air.local>
2018-02-21 15:30:05 -0600

    preliminary spotify stuff

---
+++ iOS/Music-Driven-Workout-App/BridgeViewController.m
@@ -0,0 +1,37 @@
+//
+//  BridgeViewController.m
+//  Music-Driven-Workout-App
+//
+//  Created by techbar on 2/21/18.
+//  Copyright © 2018 UChicago SoftCon. All rights reserved.
+//
+
+#import "BridgeViewController.h"
+
+@interface BridgeViewController ()
+
+@end
+
+@implementation BridgeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
