7bf61fdd8a699b4a71062c78ee270f882a1f3e52
Ama291 <ama291@gmail.com>
2018-02-25 13:36:36 -0600

    iOS update

---
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/Assets.xcassets/LoginViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LoginViewController.swift
+//  Music-Driven-Workout-App
+//
+//  Created by Alex A on 2/23/18.
+//  Copyright © 2018 UChicago SoftCon. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBOutlet weak var userBox: UITextField!
+    @IBOutlet weak var passBox: UITextField!
+    @IBOutlet weak var goButton: UIButton!
+    
+    var userID = "";
+    
+    @IBAction func goButtonClick(_ sender: Any) {
+        userID =
+            self.performSegue(withIdentifier: "loginSegue", sender: self)
+    }
+
+}
