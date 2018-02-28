159b7345f09c72be5ec45452134e34f1b9f0331b
jessica <jessicawang@uchicago.edu>
2018-02-28 17:43:13 -0600

    adding to themes UI

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesMenuViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesMenuViewController.swift
@@ -6,33 +6,116 @@
 //  Copyright © 2018 UChicago SoftCon. All rights reserved.
 //
 
+//import UIKit
+//
+//class ThemesMenuViewController: UIViewController {
+//
+//    var userid: String!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//    @IBAction func goToHome(_ sender: UIButton) {
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let vc = storyboard.instantiateViewController(withIdentifier: "homeID") as! MenuViewController
+//        vc.userid = userid!
+//        present(vc, animated: true, completion: nil)
+//    }
+//
+//}
+
+
+//
+//  GoalsMenuViewController.swift
+//  Music-Driven-Workout-App
+//
+//  Created by Christopher Choy on 2/25/18.
+//  Copyright © 2018 UChicago SoftCon. All rights reserved.
+//
+
 import UIKit
 
-class ThemesMenuViewController: UIViewController {
-
+class ThemesMenuViewController: UITableViewController {
+    
     var userid: String!
+    var tableArray = [String:Any] ()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         // Do any additional setup after loading the view.
+        populateThemes()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    struct themesResult: Codable {
+        var Result: String
+        var Status: String
     }
-    */
+    
+    func populateThemes() {
+        guard let url = URL(string: "http://138.197.49.155:8000/api/workouts/themessaved/") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let postString = "userid=" + userid + "&key=SoftCon2018"
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            guard let content = data else {
+                print("not returning data")
+                return
+            }
+            // guard let json = try? JSONDecoder().decode(goalsResult.self, from: data!) else { return }
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            // let jsonRes = json["Result"]!
+            if let array = json as? [String : Any] {
+                self.tableArray = array
+                print(self.tableArray)
+            }
+            print(self.tableArray)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            }.resume()
+    }
+    
+    
     @IBAction func goToHome(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "homeID") as! MenuViewController
@@ -40,4 +123,23 @@ class ThemesMenuViewController: UIViewController {
         present(vc, animated: true, completion: nil)
     }
     
+    @IBAction func testPopulateThemes(_ sender: UIButton) {
+        print("testing populate themes")
+        populateThemes()
+    }
+    
 }
+extension ThemesMenuViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = (self.tableArray["Result"]! as! String)
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableArray.count/2
+    }
+}
+
