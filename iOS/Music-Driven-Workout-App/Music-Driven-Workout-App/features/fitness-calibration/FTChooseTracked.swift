cb30277e2059c070eae57af178a99bb4e1ee3e93
Lucy Newman <newmanlucy@golbat.cs.uchicago.edu>
2018-02-28 11:16:46 -0600

    Working on tables

---
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/fitness-calibration/FTChooseTracked.swift
@@ -0,0 +1,45 @@
+//
+//  FTChooseTracked.swift
+//  Music-Driven-Workout-App
+//
+//  Created by Lucy Newman on 2/28/18.
+//  Copyright © 2018 UChicago SoftCon. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class FTChooseTracked: UIViewController {
+
+    var viewModel = ViewModel()
+    
+    @IBOutlet weak var tableView: UITableView?
+    @IBOutlet weak var nextButton: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView?.register(CustomCell.nib, forCellReuseIdentifier: CustomCell.identifier)
+        tableView?.estimatedRowHeight = 100
+        tableView?.rowHeight = UITableViewAutomaticDimension
+        tableView?.allowsMultipleSelection = true
+        tableView?.dataSource = viewModel
+        tableView?.delegate = viewModel
+        tableView?.separatorStyle = .none
+        
+        viewModel.didToggleSelection = { [weak self] hasSelection in
+            self?.nextButton?.isEnabled = hasSelection
+        }
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        print(viewModel.selectedItems.map { $0.title })
+        tableView?.reloadData()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
