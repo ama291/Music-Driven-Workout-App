4809c31e9a6c5011026660aad125a39a10235843
Julia Xu <juliaxu@uchicago.edu>
2018-02-28 13:39:56 -0600

    added spotify

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/LoginViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/LoginViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import SafariServices
+import AVFoundation
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAudioStreamingDelegate {
 
+    // Variables
+    var auth = SPTAuth.defaultInstance()!
+    var session:SPTSession!
+    
+    // Initialzed in either updateAfterFirstLogin: (if first time login) or in viewDidLoad (when there is a check for a session object in User Defaults
+    var player: SPTAudioStreamingController?
+    var loginUrl: URL?
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setup()
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.updateAfterFirstLogin), name: NSNotification.Name(rawValue: "loginSuccessfull"), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +35,67 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setup () {
+        // insert redirect your url and client ID below
+        let redirectURL = "Music-Driven-Workout-App://returnAfterLogin" // put your redirect URL here
+        //_ = "8f81031574b54170a24a3a1afab27578" // put your client ID here
+        auth.redirectURL     = URL(string: redirectURL)
+        auth.clientID        = "8f81031574b54170a24a3a1afab27578"
+        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
+        loginUrl = auth.spotifyWebAuthenticationURL()
+    }
+    
+    func initializaPlayer(authSession:SPTSession){
+        if self.player == nil {
+            self.player = SPTAudioStreamingController.sharedInstance()
+            self.player!.playbackDelegate = self
+            self.player!.delegate = self
+            try! player?.start(withClientId: auth.clientID)
+            self.player!.login(withAccessToken: authSession.accessToken)
+        }
+    }
+    
+    @objc func updateAfterFirstLogin () {
+        
+        loginButton.isHidden = true
+        let userDefaults = UserDefaults.standard
+        
+        if let sessionObj:AnyObject = userDefaults.object(forKey: "SpotifySession") as AnyObject? {
+            
+            let sessionDataObj = sessionObj as! Data
+            let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
+            
+            self.session = firstTimeSession
+            initializaPlayer(authSession: session)
+            self.loginButton.isHidden = true
+            // self.loadingLabel.isHidden = false
+        }
+    }
+    
+    func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+        // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
+        print("logged in")
+        self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+            if (error != nil) {
+                print("playing!")
+            }
+            
+        })
+        
+    }
+    
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+        //     UIApplication.shared.open(loginUrl!, options: nil, completionHandler: nil)
+        
+        if UIApplication.shared.openURL(loginUrl!) {
+            
+            if auth.canHandle(auth.redirectURL) {
+                // To do - build in error handling
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
