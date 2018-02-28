bdbf394bb44c637c89e9fb7b2c688f05f3b90bdd
Ama291 <ama291@gmail.com>
2018-02-27 20:33:02 -0600

    work exercise view

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/workout/WorkExerciseViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/workout/WorkExerciseViewController.swift
@@ -12,10 +12,19 @@ class WorkExerciseViewController: UIViewController {
     
     var userid: String!
     
+    //variables to be taken from workout summary
+    var exercisenames: [String]!
+    var exercisedescriptions: [String]!
+    var exercisedurations: [Int]!
+    var exerciseimages: [String]!
+    
+    var heartrate = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        startWorkouts()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,5 +53,33 @@ class WorkExerciseViewController: UIViewController {
     @IBOutlet weak var skipbutton: UIButton!
     @IBOutlet weak var eximage: UIImageView!
     
+    @objc func startWorkouts() {
+        namelabel.adjustsFontSizeToFitWidth = true
+        descriptionlabel.lineBreakMode = .byWordWrapping
+        descriptionlabel.numberOfLines = 0
+        //test values
+        exercisenames = ["Lying Face Down Plate Neck Resistance", "Lying Face Up Plate Neck Resistance"]
+        exercisedescriptions = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Lorem ipsum dolor sit amet, consectetur adipiscing elit."]
+        exercisedurations = [30, 30]
+        exerciseimages = ["https://www.bodybuilding.com/exercises/exerciseImages/sequences/25/Male/m/25_2.jpg","https://www.bodybuilding.com/exercises/exerciseImages/sequences/26/Male/m/26_1.jpg"]
+        
+        var i = 0
+        /*for _ in exercisenames {
+            doexercise(index: i)
+            i += 1
+        }*/
+        doexercise(index: 0)
+    }
+    
+    @objc func doexercise(index: Int) {
+        namelabel.text = exercisenames[index]
+        descriptionlabel.text = exercisedescriptions[index]
+        timelabel.text = "Time Remaining: " + String(exercisedurations[index]) + "s"
+        let url = URL(string: exerciseimages[index])
+        let data = try? Data(contentsOf: url!)
+        eximage.image = UIImage(data: data!)
+        eximage.contentMode = UIViewContentMode.scaleAspectFit
+        
+    }
     
 }
