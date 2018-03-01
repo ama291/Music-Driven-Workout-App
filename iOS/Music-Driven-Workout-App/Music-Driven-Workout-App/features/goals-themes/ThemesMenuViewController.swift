493fad7accf2a8e107f14095e23d8efa10295f48
jessica <jessicawang@uchicago.edu>
2018-03-01 01:06:24 -0600

    editing themes UI

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesMenuViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesMenuViewController.swift
@@ -1,49 +1,4 @@
 //
-//  ThemesMenuViewController.swift
-//  Music-Driven-Workout-App
-//
-//  Created by Christopher Choy on 2/25/18.
-//  Copyright © 2018 UChicago SoftCon. All rights reserved.
-//
-
-//import UIKit
-//
-//class ThemesMenuViewController: UIViewController {
-//
-//    var userid: String!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destinationViewController.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//    @IBAction func goToHome(_ sender: UIButton) {
-//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-//        let vc = storyboard.instantiateViewController(withIdentifier: "homeID") as! MenuViewController
-//        vc.userid = userid!
-//        present(vc, animated: true, completion: nil)
-//    }
-//
-//}
-
-
-//
 //  GoalsMenuViewController.swift
 //  Music-Driven-Workout-App
 //
