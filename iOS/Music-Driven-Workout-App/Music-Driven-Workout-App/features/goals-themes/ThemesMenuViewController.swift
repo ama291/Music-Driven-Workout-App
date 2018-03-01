491a16271d6bcc3e9d63bf614ce53d01748f9249
jessica <jessicawang@uchicago.edu>
2018-03-01 04:33:40 -0600

    Made themes prettier

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesMenuViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesMenuViewController.swift
@@ -1,43 +1,101 @@
 //
-//  ThemesMenuViewController.swift
+//  GoalsMenuViewController.swift
 //  Music-Driven-Workout-App
 //
 //  Created by Christopher Choy on 2/25/18.
 //  Copyright © 2018 UChicago SoftCon. All rights reserved.
 //
-
 import UIKit
 
-class ThemesMenuViewController: UIViewController {
-
+class ThemesMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
     var userid: String!
+    var tableArray = [String:Any] ()
+    
+    var passedUserId = String()
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         // Do any additional setup after loading the view.
+        populateThemes()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    struct themesResult: Codable {
+        var Result: String
+        var Status: String
     }
-    */
+    
+    func populateThemes() {
+        guard let url = URL(string: "http://138.197.49.155:8000/api/workouts/themessaved/") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        let postString = "userid=" + userid + "&key=SoftCon2018"
+        passedUserId = userid
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            guard let content = data else {
+                print("not returning data")
+                return
+            }
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+                print("Not containing JSON")
+                return
+            }
+            if let array = json as? [String : Any] {
+                self.tableArray = array
+                print(self.tableArray)
+            }
+            print(self.tableArray)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            }.resume()
+    }
+    
+    
     @IBAction func goToHome(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "homeID") as! MenuViewController
         vc.userid = userid!
         present(vc, animated: true, completion: nil)
+    }
+    
+    @IBAction func goToAddTheme(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "goals-themes", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "themesAdd") as! MenuViewController
+        vc.userid = userid!
+        present(vc, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = (self.tableArray["Result"]! as! String)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableArray.count/2
     }
     
 }
