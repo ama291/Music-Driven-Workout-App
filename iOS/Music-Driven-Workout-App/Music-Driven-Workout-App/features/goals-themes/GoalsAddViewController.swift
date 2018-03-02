a5261e660c96020d614ad103f03fc29be1ae5f9f
jessica <jessicawang@uchicago.edu>
2018-03-01 19:57:45 -0600

    Working on integrating addgoal api

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/GoalsAddViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/GoalsAddViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class GoalsAddViewController: UIViewController {
     
     var userid: String!
+    var name: String!
+    var dsc: String!
+    var goalnum: String!
+    var categories: String!
+    var musclegroups: String!
+    var duration: String!
+    var daysperweek: String!
+    var notify: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,7 +62,34 @@ class GoalsAddViewController: UIViewController {
 //        navigationController?.pushViewController(myVC, animated: true)
 //    }
     
+    @objc func addGoalAPI() {
+        guard let url = URL(string: "http://138.197.49.155:8000/api/goals/addgoal/") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        //userid, name, description, goalnum, categories, musclegroups, duration, dayssperweek, notify, key
+        let postString = "userid=" + userid + "name=" + name
+        let postString2 = postString + "description=" + dsc + "goalnum=" + goalnum + "categories=" + categories
+        let postString3 = postString2 + "musclegroups=" + musclegroups + "duration=" + duration
+        let postString4 = postString3 + "daysperweek=" + daysperweek + "notify=" + notify + "&key=SoftCon2018"
+        request.httpBody = postString4.data(using: String.Encoding.utf8)
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                guard let json = try? JSONDecoder().decode(jsonRequest.self, from: data) else { return }
+                self.userid = json.Result
+                print(self.userid)
+            }
+
+        }.resume()
+    }
+    
     @IBAction func goBackToGoalsMenu(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "goals-themes", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "goalsID") as! GoalsMenuViewController
+        vc.userid = userid!
+        present(vc, animated: true, completion: nil)
+    }
+    @IBAction func goBackToGoalsHome(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "goals-themes", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "goalsID") as! GoalsMenuViewController
         vc.userid = userid!
