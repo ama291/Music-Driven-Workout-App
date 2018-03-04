afe9b7bf3edaaa2c9dc0d37d5aa7293e00a8bfd5
Julia Xu <juliaxu@uchicago.edu>
2018-03-04 14:40:31 -0600

    themes spotify integration

--- iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesAddViewController.swift
+++ iOS/Music-Driven-Workout-App/Music-Driven-Workout-App/features/goals-themes/ThemesAddViewController.swift
@@ -8,13 +8,79 @@
 
 import UIKit
 
-class ThemesAddViewController: UIViewController {
+class ThemesAddViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "themescell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "blah"
+        return cell
+    }
     
     var userid: String!
+    var tableArray: [String: Dictionary<String, Any>] = [:]
+    var token: String!
     
+    @IBOutlet weak var artistToSearchFor: UITextField!
+    @IBAction func searchForTheme(_ sender: UIButton) {
+        print("token")
+        print(token)
+        let postString = "q=" + artistToSearchFor.text! + "&type=artist&limit=5"
+        guard let url = URL(string: "https://api.spotify.com/v1/search?" + postString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Authorization: Bearer " + token, forHTTPHeaderField: "Authorization")
+      
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            guard let content = data else {
+                print("not returning data")
+                return
+            }
+            guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) else {
+                print("Not containing JSON")
+                return
+            }
+            if let array = json as? [String : Dictionary<String,Any>] {
+                self.tableArray = array
+            }
+//            print("spotify result")
+//            print(self.tableArray["artists"])
+            print("tyring")
+//            print(self.tableArray.count)
+            if let names = self.tableArray["artists"]!["items"] as? NSArray {
+                for item in names {
+//                    print(item)
+//                    print((item as AnyObject).count)
+//                    print(type(of:item))
+                    if let item2 = item as? NSDictionary {
+                        print(item2["name"])
+                    }
+                }
+            }
+//            for (name, artist) in self.tableArray["artists"]!{
+//                if(name == "items") {
+//                    for item in (self.tableArray["artists"]!["items"]) {
+//                        print(item)
+//                    }
+//                } else {
+//                    continue
+//                }
+//            }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            }.resume()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "themescell")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -27,6 +93,7 @@ class ThemesAddViewController: UIViewController {
         let storyboard = UIStoryboard(name: "goals-themes", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "themesID") as! ThemesMenuViewController
         vc.userid = userid!
+        vc.token = token!
         present(vc, animated: true, completion: nil)
     }
     /*
